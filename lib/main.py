f6eeccb8be6ff0706a9a0ed47ef1ec0272841442
caio_ingber <caioingber@gmail.com>
2020-03-20 10:57:35 -0400

    added show all functionality

--- lib/main.py
+++ lib/main.py
@@ -33,21 +33,39 @@ class Interface:
         self.length = 0
     
     def intro(self):
-        option = input("Welcome to the Contact Book. Choose from the following options: \n (a) Look for a contact\n (b) Add a new contact\n")
+        option = input("""Welcome to the Contact Book. Choose from the following options: \n 
+        (a) Look for a contact\n 
+        (b) Add a new contact\n
+        (c) Show all contacts""")
         if option == 'a':
             self.search()
         elif option == 'b':
             self.create()
+        elif option == 'c':
+            self.show_all()
         else:
             self.invalid()
+
+    def show_all(self):
+        query = Contact.select()
+        query = list(query)
+        for contact in query:
+            print(f"{query.index(contact) + 1}. Name: {contact.first_name} {contact.last_name}\n Number: {contact.phone_number}\n")
+
 
     def search(self):
         search_name = input("Please Enter a First Name: ")
         query = Contact.select().where(Contact.first_name ** f"%{search_name}%")
         if query.exists():
             query = list(query)
-            for contact in query:
-                print(f" Name: {contact.first_name} {contact.last_name}\n Number: {contact.phone_number}\n")
+            if len(query) > 1:
+                for contact in query:
+                    print(f"{query.index(contact) + 1}. Name: {contact.first_name} {contact.last_name}\n Number: {contact.phone_number}\n")
+                return query
+            else:
+                for contact in query:
+                    print(f"Name: {contact.first_name} {contact.last_name}\nNumber: {contact.phone_number}\n")
+                return query
         else:
             cant_find = input("Sorry, we couldn't find that contact. Wan't to do another search? Y/N ")
             if cant_find == 'Y':
